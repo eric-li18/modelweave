d5b4ea11e3eb2dc53673a14932f20fb9814e0424
Eric <erickh.li@mail.utoronto.ca>
2019-09-12 22:49:37 -0400

    added regression on testing data

--- regression.py
+++ regression.py
@@ -21,4 +21,30 @@ plt.title('SAT vs. GPA Scores (Training Data)')
 plt.show()
 
 
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train,regression.predict(X_train), color='green')
+plt.xlabel('SAT Scores')
+plt.ylabel('GPA (4.0 scale)')
+plt.title('SAT vs. GPA Scores (Testing Data)')
+plt.show()
 
+
+X_pred = []
+i=1
+while(i):
+    input_data=input("Enter a SAT Score (or enter any non integer to exit): ")
+    if (input_data.isnumeric() and int(input_data) <=2400):
+        X_pred.append([int(input_data)])
+    else:
+        i=0
+
+y_pred = regression.predict(X_pred)
+print(y_pred)
+
+#Unblock this code to check that the predictions are correct in relation to the regression line
+""" plt.scatter(X_pred, y_pred, color='red')
+plt.plot(X_train,regression.predict(X_train), color='green')
+plt.xlabel('SAT Scores')
+plt.ylabel('GPA (4.0 scale)')
+plt.title('SAT vs. GPA Scores (Prediction Data)')
+plt.show() """
