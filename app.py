94425e0d4af5738b48ae66ed8e4e226dcc0be3a2
Eric <erickh.li@mail.utoronto.ca>
2020-05-01 21:59:06 -0400

    moved files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-import polyreg
-import logreg
+import files/polyreg
+import files/logreg
 from sklearn import model_selection, datasets
 import numpy as np
 import pandas as pd
