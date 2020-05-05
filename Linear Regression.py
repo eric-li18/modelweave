8ced255f1fe7fba242fd122b32b75d91a4e4892e
Eric <erickh.li@mail.utoronto.ca>
2020-05-04 20:11:10 -0400

    formatted linear regression file

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,11 +1,11 @@
-#================================================================================================================
-#----------------------------------------------------------------------------------------------------------------
+# ================================================================================================================
+# ----------------------------------------------------------------------------------------------------------------
 #									SIMPLE LINEAR REGRESSION
-#----------------------------------------------------------------------------------------------------------------
-#================================================================================================================
+# ----------------------------------------------------------------------------------------------------------------
+# ================================================================================================================
 
-#Simple linear regression is applied to stock data, where the x values are time and y values are the stock closing price.
-#This is not an ideal application of simple linear regression, but it suffices to be a good experiment.
+# Simple linear regression is applied to stock data, where the x values are time and y values are the stock closing price.
+# This is not an ideal application of simple linear regression, but it suffices to be a good experiment.
 
 import math
 import numpy as np
@@ -13,110 +13,113 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas
 import datetime
-
-#Quandl for getting stock data
 import quandl
 
-#for plotting
+# for plotting
 plt.style.use('ggplot')
 
 class CustomLinearRegression:
-	
-	def __init__(self):
-		self.intercept = 0
-		self.slope = 0
 
-	#arithmetic mean
-	def am(self, arr):
-		tot = 0.0
-		for i in arr:
-			tot+= i
-		return tot/len(arr)
+    def __init__(self):
+        self.intercept = 0
+        self.slope = 0
 
-	#finding the slope in best fit line
-	def best_fit(self, dimOne, dimTwo):
-		self.slope = ( (self.am(dimOne) * self.am(dimTwo) ) - self.am(dimOne*dimTwo) ) / ( self.am(dimOne)**2 - self.am(dimOne**2) ) #formula for finding slope
-		return self.slope
+    # arithmetic mean
+    def arithmetic_mean(self, arr):
+        total = 0.0
+        for i in arr:
+            total += i
+        return total/len(arr)
 
-	#finding the best fit intercept
-	def y_intercept(self, dimOne ,dimTwo):
-		self.intercept = self.am( dimTwo ) - ( self.slope * self.am(dimOne) )
-		return self.intercept
+    # finding the slope in best fit line
+    def best_fit(self, dimOne, dimTwo):
+        self.slope = ((self.arithmetic_mean(dimOne) * self.arithmetic_mean(dimTwo)) - self.arithmetic_mean(dimOne*dimTwo)) / \
+            (self.arithmetic_mean(dimOne)**2 - self.arithmetic_mean(dimOne**2))  # formula for finding slope
+        return self.slope
 
-	#predict for future values based on model
-	def predict(self, ip):
-		ip = np.array(ip)
-		predicted = [(self.slope*param) + self.intercept for param in ip] #create a "predicted" array where the index corresponds to the index of the input
-		return predicted
-		
-	#find the squared error
-	def squared_error(self, original, model):
-		return sum((model - original) **2)
+    # finding the best fit intercept
+    def y_intercept(self, dimOne, dimTwo):
+        self.intercept = self.arithmetic_mean(dimTwo) - (self.slope * self.arithmetic_mean(dimOne))
+        return self.intercept
 
-	#find co-efficient of determination for R^2
-	def cod(self, original, model):
-		am_line = [self.am(original) for y in original]
-		sq_error = self.squared_error(original, model)
-		sq_error_am = self.squared_error(original, am_line)
-		return 1 - (sq_error/sq_error_am) #R^2 is nothing but 1 - of squared error for our model / squared error if the model only consisted of the mean
+    # predict for future values based on model
+    def predict(self, ip):
+        ip = np.array(ip)
+        # create a "predicted" array where the index corresponds to the index of the input
+        predicted = [(self.slope*param) + self.intercept for param in ip]
+        return predicted
+
+    # find the squared error
+    def squared_error(self, original, model):
+        return sum((model - original) ** 2)
+
+    # find co-efficient of determination for R^2
+    def r_squared(self, original, model):
+        am_line = [self.arithmetic_mean(original) for y in original]
+        sq_error = self.squared_error(original, model)
+        sq_error_am = self.squared_error(original, am_line)
+        # R^2 is nothing but 1 - of squared error for our model / squared error if the model only consisted of the mean
+        return 1 - (sq_error/sq_error_am)
+
 
 def main():
-	stk = quandl.get("WIKI/TSLA")
+    quandl.ApiConfig.api_key = ""
+    stk = quandl.get("WIKI/TSLA", start_date=, end_date=datetime.)
 
-	simpl_linear_regression = CustomLinearRegression()
+    model = CustomLinearRegression()
 
-	#reset index to procure date - date was the initial default index
-	stk = stk.reset_index()
-	
-	#Add them headers
-	stk = stk[['Date','Adj. Open','Adj. High','Adj. Low','Adj. Close', 'Volume']]
+    # reset index to procure date - date was the initial default index
+    stk = stk.reset_index()
 
-	stk['Date'] = pandas.to_datetime(stk['Date'])    
-	stk['Date'] = (stk['Date'] - stk['Date'].min())  / np.timedelta64(1,'D')
+    # Add them headers
+    stk = stk[['Date', 'Adj. Open', 'Adj. High',
+               'Adj. Low', 'Adj. Close', 'Volume']]
 
+    stk['Date'] = pandas.to_datetime(stk['Date'])
+    stk['Date'] = (stk['Date'] - stk['Date'].min()) / np.timedelta64(1, 'D')
 
-	#The column that needs to be forcasted using linear regression
-	forecast_col = 'Adj. Close'
-	
-	#take care of NA's
-	stk.fillna(-999999, inplace = True)
-	stk['label'] = stk[forecast_col]
+    # The column that needs to be forcasted using linear regression
+    forecast_col = 'Adj. Close'
 
+    # take care of NA's
+    stk.fillna(-999999, inplace=True)
+    stk['label'] = stk[forecast_col]
 
-	#IN CASE THE INPUT IS TO BE TAKEN IN FROM THE COMMAND PROMPT UNCOMMENT THE LINES BELOW
+    # IN CASE THE INPUT IS TO BE TAKEN IN FROM THE COMMAND PROMPT UNCOMMENT THE LINES BELOW
 
-	#takes in input from the user
-	#x = list(map(int, input("Enter x: \n").split()))
-	#y = list(map(int, input("Enter y: \n").split()))
+    # takes in input from the user
+    #x = list(map(int, input("Enter x: \n").split()))
+    #y = list(map(int, input("Enter y: \n").split()))
 
-	#convert to an numpy array with datatype as 64 bit float.
-	#x = np.array(x, dtype = np.float64)
-	#y = np.array(y, dtype = np.float64)
+    # convert to an numpy array with datatype as 64 bit float.
+    #x = np.array(x, dtype = np.float64)
+    #y = np.array(y, dtype = np.float64)
 
-	stk.dropna(inplace = True)
+    stk.dropna(inplace=True)
 
-	x = np.array(stk['Date'])
-	y = np.array(stk['label'])
+    x = np.array(stk['Date'])
+    y = np.array(stk['label'])
 
-	#Always in the order: first slope, then intercept
-	slope = simpl_linear_regression.best_fit(x, y) #find slope
-	intercept = simpl_linear_regression.y_intercept(x, y) #find the intercept
+    # Always in the order: first slope, then intercept
+    slope = model.best_fit(x, y)  # find slope
+    intercept = model.y_intercept(x, y)  # find the intercept
 
-	ip = list(map(int, input("Enter x to predict y: \n").split()))
+    ip = list(map(int, input("Enter x to predict y: \n").split()))
 
-	line = simpl_linear_regression.predict(ip) #predict based on model
-	
-	reg = [(slope*param) + intercept for param in x]
+    line = model.predict(ip)  # predict based on model
 
-	print("Predicted value(s) after linear regression :", line)
+    reg = [(slope*param) + intercept for param in x]
 
-	r_sqrd = simpl_linear_regression.cod(y, reg)
-	print("R^2 Value: " ,r_sqrd)
-	
-	plt.scatter(x, y)
-	plt.scatter(ip, line, color = "red")
-	plt.plot(x, reg)
-	plt.show()
+    print("Predicted value(s) after linear regression :", line)
+
+    r_sqrd = model.r_squared(y, reg)
+    print("R^2 Value: ", r_sqrd)
+
+    plt.scatter(x, y)
+    plt.scatter(ip, line, color="red")
+    plt.plot(x, reg)
+    plt.show()
+
 
 if __name__ == "__main__":
-	main()
+    main()
